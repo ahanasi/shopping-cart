7c0410f74dda3570d442fc27353d85bba6cd79c2
XXX Ahana <ahanasi@seas.upenn.edu>
2022-02-25 18:01:12 -0800

    Prevent negative item quantities from inputs

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -4,20 +4,41 @@ const Shop = () => {
   const [products, setProducts] = useState([]);
   const [cart, setCart] = useState([]);
 
+  const sanitizeQuantity = (id) => {
+    const quant = document.getElementById(id).value;
+    let sanitizedInput;
+
+    if (parseInt(quant) < 0) {
+      sanitizedInput = 0;
+    } else {
+      sanitizedInput = parseInt(quant);
+    }
+    clearInput(id);
+    return sanitizedInput;
+  };
+
+  const clearInput = (id) => {
+    document.getElementById(id).value = '';
+  };
+
   const addToCart = (e) => {
     const productID = e.target.dataset.id;
     const item = products[productID - 1];
 
+    const itemQuantity = sanitizeQuantity(productID);
+
     if (cart.some((item) => item.id == productID)) {
       setCart((prevState) =>
         prevState.map((product) =>
-          product.id == productID ? { ...product, quantity: product.quantity + 1 } : product
+          product.id == productID
+            ? { ...product, quantity: product.quantity + itemQuantity }
+            : product
         )
       );
     } else {
       setCart((prevState) => [
         ...prevState,
-        { id: item.id, title: item.title, price: item.price, quantity: 1 }
+        { id: item.id, title: item.title, price: item.price, quantity: itemQuantity }
       ]);
     }
   };
@@ -31,10 +52,15 @@ const Shop = () => {
   }, []);
 
   useEffect(() => {
-    document.addEventListener('click', addToCart);
+    const btns = document.getElementsByClassName('addBtn');
+    for (let index = 0; index < btns.length; index++) {
+      btns[index].addEventListener('click', addToCart);
+    }
 
     return () => {
-      document.removeEventListener('click', addToCart);
+      for (let index = 0; index < btns.length; index++) {
+        btns[index].removeEventListener('click', addToCart);
+      }
     };
   }, [addToCart]);
 
@@ -58,7 +84,8 @@ const Shop = () => {
             </div>
           </div>
           <footer className="card-footer">
-            <button className="card-footer-item" data-id={product.id}>
+            <input id={product.id} type="number" min="0" step={1} />
+            <button className="card-footer-item addBtn" data-id={product.id}>
               Add to Cart
             </button>
           </footer>
